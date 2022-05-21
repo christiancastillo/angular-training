842106505db3e02101097d1242685cd849fe1e16
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-05-20 21:27:36 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,34 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ClienteService } from './clientes/cliente.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { FooterComponent } from './footer/footer.component'
+
 
 const ROUTES: Routes = [
   //{path: '', redirectTo: '/clientes', pathMatch: 'full'}, //cuando se carga la pagina por default entramos aqui
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'usuario', component: UsuarioComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ClientesComponent
+    ClientesComponent,
+    UsuarioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(ROUTES),
-    BrowserAnimationsModule //arreglo de rutas
+    BrowserAnimationsModule, //arreglo de rutas
+    MatSliderModule
+  ],
+  exports: [
+    MatSliderModule
   ],
   providers: [ClienteService], //servicios se van a incluir en la app, se pueden incluir con comas
   bootstrap: [AppComponent]
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,17 @@
+import { MatSliderModule } from '@angular/material/slider'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule
+  ],
+  exports: [
+    MatSliderModule
+  ]
+})
+export class AppModule { }
